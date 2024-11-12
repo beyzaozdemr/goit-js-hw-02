9d94dc0fbe2229ecff14149a2b1199cc1e1153b2
Beyza Özdemir <beyzazdmr@hotmail.com>
2024-11-12 22:33:14 +0300

    4th task edited

--- js/task-4.js
+++ js/task-4.js
@@ -1,16 +1,22 @@
 function getShippingCost(country) {
+  let cost;
   switch (country) {
     case "China":
-      return "Shipping to China will cost 100 credits";
+      cost = 100;
+      break;
     case "Chile":
-      return "Shipping to Chile will cost 150 credits";
+      cost = 250;
+      break;
     case "Australia":
-      return "Shipping to Australia will cost 170 credits";
+      cost = 170;
+      break;
     case "Jamaica":
-      return "Shipping to Jamaica will cost 120 credits";
+      cost = 120;
+      break;
     default:
       return "Sorry, there is no delivery to your country";
   }
+  return `Shipping to ${country} will cost ${cost} credits`;
 }
 console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
 console.log(getShippingCost("Germany")); // "Sorry, there is no delivery to your country"
